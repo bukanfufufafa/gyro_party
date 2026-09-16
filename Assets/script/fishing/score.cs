e6237ea69d78f112eec7b3fbe99569954a373e74
bukanfufufafa <nabilrizky249@gmail.com>
2026-09-16 22:23:41 +0700

    tes

---
+++ Assets/script/fishing/score.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class scorefishing : MonoBehaviour
+{
+    public TextMeshProUGUI player1Text;
+    public TextMeshProUGUI player2Text;
+
+    public fishing player1;
+    public fishing player2;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Text.text = "0";
+        player2Text.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player1Text.text = "" + player1.score;
+        player2Text.text = "" + player2.score;
+    }
+
+  
+}
