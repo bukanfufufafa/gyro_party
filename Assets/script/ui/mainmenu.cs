79efbfdcc51dd05c30501230c893a09f617070ac
bukanfufufafa <nabilrizky249@gmail.com>
2026-09-17 01:46:12 +0700

    paus

--- Assets/script/ui/mainmenu.cs
+++ Assets/script/ui/mainmenu.cs
@@ -1,40 +1,112 @@
-using System.Buffers;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class mainmenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Main Menu")]
     [SerializeField] private GameObject gamemode;
     [SerializeField] private GameObject settings;
     [SerializeField] private GameObject credit;
     [SerializeField] private GameObject quit;
-    
-    void Start()
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pause;
+
+    private bool paused = false;
+
+    private void Awake()
     {
-        
+        // Pastikan script ini tetap aktif
+        Debug.Log("MAINMENU SCRIPT AKTIF");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Time.timeScale = 1f;
+        paused = false;
+
+        if (pause != null)
+        {
+            pause.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC TERDETEKSI");
+
+            if (paused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("PAUSE GAME DIPANGGIL");
+
+        paused = true;
+
+        if (pause != null)
+        {
+            pause.SetActive(true);
+        }
+
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("RESUME BUTTON DIPANGGIL");
+
+        Time.timeScale = 1f;
+        paused = false;
+
+        if (pause != null)
+        {
+            pause.SetActive(false);
+        }
+    }
+
+    public void goMenu()
+    {
+        Debug.Log("GO MENU BUTTON DIPANGGIL");
+
+        Time.timeScale = 1f;
+        paused = false;
+
+        SceneManager.LoadScene("mainmenu");
     }
 
     public void cutting()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("cutting");
     }
 
     public void fishing()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("fishing");
     }
 
     public void plane()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("airplane");
+    }
+
+    public void exit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
     }
 }
